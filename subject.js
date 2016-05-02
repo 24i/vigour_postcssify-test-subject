877f42317bc76d1aa98d599986e4b28fbc82515d
Shawn Inder <shawn@vigour.io>
2016-05-01 20:45:59 -0400

    first draft

---
+++ subject.js
@@ -0,0 +1,9 @@
+'use strict'
+
+require('./subject.css')
+
+require('postcssify-test-subject-dep-dep')
+require('postcssify-test-subject-dep')
+require('postcssify-test-subject-dep2')
+
+module.exports = exports = 'subject'
